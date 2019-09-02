be0c8fcda57a30617368b557e750fce0ddb49f2f
Pavel <pashka_89@mail.ru>
2019-09-02 14:05:09 +0300

    - верстка до конца; - валидация html;

--- Bouncy/js/menu.js
+++ Bouncy/js/menu.js
@@ -1,5 +1,6 @@
 const menuBtn = document.querySelector('.navbar_show-btn');
 const menu = document.querySelector('.navbar_menu');
+const [...navMain] = document.querySelectorAll('.navbar_menu > li > a');
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -9,4 +10,14 @@ document.addEventListener('DOMContentLoaded', () => {
       menu.style.maxHeight = '500px':
       menu.removeAttribute('style');
   })
+
+  // плавная прокрутка к якорю
+  navMain.forEach((item) => {
+      item.addEventListener('click', (e) => {
+          e.preventDefault();
+          const href = e.target.getAttribute('href');
+          const aim = document.querySelector(href);
+          aim.scrollIntoView({block: 'start', behavior: 'smooth'});
+      })
+  });
 })
--- Bouncy/js/subscribe.js
+++ Bouncy/js/subscribe.js
@@ -1,21 +1,7 @@
-const mailPattern = /^[^\s^@]+@[a-z]+\.(ru|com|org)/;
-
 const submitSubscribe = document.querySelector('.subsribe_submit');
 const inputSubscribe = document.querySelector('.subscribe_mail');
 
 document.addEventListener('DOMContentLoaded', () => {
-  submitSubscribe.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (!(mailPattern.test(inputSubscribe.value))) {
-      inputSubscribe.classList.add('error-input');
-    }
-  })
-
-  inputSubscribe.addEventListener('focus', (e) => {
-    e.preventDefault();
-    inputSubscribe.classList.contains('error-input') ?
-    inputSubscribe.classList.remove('error-input') :
-    null;
-  })
+  emailVerification(submitSubscribe, inputSubscribe);
 })
   
