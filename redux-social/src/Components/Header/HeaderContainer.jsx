d892de6c51237b9f86fd40cfe09091de04f74ddb
PavelK <pashka_89@mail.ru>
2020-05-26 13:25:13 +0300

    - Исправлена загрузка залогиненного пользователя;

--- redux-social/src/Components/Header/HeaderContainer.jsx
+++ redux-social/src/Components/Header/HeaderContainer.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import Header from "./Header";
 import { connect } from "react-redux";
-import { getAuthProfile } from "../../Redux/authReducer";
+import { checkAuthProfile } from "../../Redux/authReducer";
+import { getProfile } from "../../Redux/profileReducer";
 
 class HeaderContainer extends React.Component {
   componentDidMount() {
-    this.props.getAuthProfile();
+    this.props.checkAuthProfile().then(() => {
+      if (this.props.isLogged) {
+        this.props.getProfile(this.props.userId);
+      }
+    });
   }
 
   render() {
-    return <Header  profile ={this.props.profile} />;
+    return <Header profile={this.props.profile} />;
   }
 }
 
 const mapStateToProps = (state) => ({
-  profile: state.auth.authProfile,
+  userId: state.auth.id,
+  profile: state.profile.profile,
   isLogged: state.auth.isLogged,
 });
 
 const mapDispatchToProps = {
-  getAuthProfile,
+  checkAuthProfile,
+  getProfile,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HeaderContainer);
--- redux-social/src/Redux/authReducer.js
+++ redux-social/src/Redux/authReducer.js
@@ -1,9 +1,7 @@
 import { API } from "../api/api";
 
 // Const
-const TOGGLE_LOGGING = "TOGGLE_LOGGING";
-const GET_USER_DATA = "GET_USER_DATA";
-const GET_AUTH_PROFILE = "GET_AUTH_PROFILE";
+const SET_USER_DATA = "GET_USER_DATA";
 
 // State
 const initialState = {
@@ -11,25 +9,15 @@ const initialState = {
   email: null,
   login: null,
   isLogged: false,
-  authProfile: null,
 };
 
 // Reducer
 export const authReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_USER_DATA:
+    case SET_USER_DATA:
       return {
         ...state,
         ...action.data,
-      };
-    case GET_AUTH_PROFILE:
-      return {
-        ...state,
-        authProfile: action.profile,
-      };
-    case TOGGLE_LOGGING:
-      return {
-        ...state,
         isLogged: true,
       };
     default:
@@ -39,29 +27,16 @@ export const authReducer = (state = initialState, action) => {
 
 // ActionCreactors
 export const setUserData = (id, email, login) => ({
-  type: GET_USER_DATA,
+  type: SET_USER_DATA,
   data: { id, email, login },
 });
-export const setAuthProfile = (profile) => ({ type: GET_AUTH_PROFILE, profile });
-export const toggleLogging = () => ({ type: TOGGLE_LOGGING });
 
 // ThunkCreactors
-export const getAuthProfile = () => (dispatch) => {
-  return API.authCheck()
-  .then((data) => {
+export const checkAuthProfile = () => (dispatch) => {
+  return API.authCheck().then((data) => {
     if (data.resultCode === 0) {
       const { id, email, login } = data.data;
       dispatch(setUserData(id, email, login));
-      dispatch(toggleLogging());
-      return id;
     }
-  })
-  .then((id) => {
-    if (id) {
-      API.getProfile(id)
-      .then((data) => {
-        dispatch(setAuthProfile(data));
-      });
-    }
-  })
-}
+  });
+};
