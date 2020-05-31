08a942bfc1b639c128c7ebeee1cd916d76440447
Pavel <pashka_89@mail.ru>
2020-05-31 13:36:36 +0300

    - Кнопки в разделе портфолио; - Переключение между работами в разделе портфолио;

--- MyResume (Bootstrap)/js/portfolio.js
+++ MyResume (Bootstrap)/js/portfolio.js
@@ -1,8 +1,9 @@
 $(document).ready(() => {
-  
-  const myWorks = $('.portfolio-item');
-  const details = $('.portfolio-details').clone();
-  const images = $('.portfolio-img').clone();
+  const buttons = $(".portfolio-btn");
+  const myWorks = $(".portfolio-item");
+  const works = $(".portfolio-wrap");
+  const details = $(".portfolio-details").clone();
+  const images = $(".portfolio-img").clone();
   let sliderItems = [];
 
   // Добавление элементов в карусель
@@ -12,22 +13,80 @@ $(document).ready(() => {
     // Добавление информации в элемент
     newItem.append(images[i], details[i]);
     // Добавление элемента в карусель
-    $('.carousel-inner').append(newItem);
+    $(".carousel-inner").append(newItem);
     // Замена классов для стилей в карусели
-    $(details[i]).addClass('carousel-caption d-md-block');
-    $(details[i]).removeClass('portfolio-details');
+    $(details[i]).addClass("carousel-caption d-md-block");
+    $(details[i]).removeClass("portfolio-details");
     // Показать ссылку
-    $(`.carousel-caption > a:eq(${i})`).removeClass('invisible');
+    $(`.carousel-caption > a:eq(${i})`).removeClass("invisible");
 
-    sliderItems = [...sliderItems, newItem ]; 
-    
+    sliderItems = [...sliderItems, newItem];
+
     $(work).click(() => {
       // Удаление "active" для предотвращения наложения
       // при повторных кликах
-      $(sliderItems[i]).siblings('.carousel-item').removeClass('active');
+      $(sliderItems[i]).siblings(".carousel-item").removeClass("active");
       // Включение "нужного" слайда
-      $(sliderItems[i]).hasClass('active') ? null : $(sliderItems[i]).addClass('active');
-    })
-  })
+      $(sliderItems[i]).hasClass("active")
+        ? null
+        : $(sliderItems[i]).addClass("active");
+    });
+  });
 
+
+  const toggleView = (className = "portfolio-wrap") => {
+    $(works).each((i, work) => {
+      $(work).addClass("hidden");
+      //TODO Promise
+      setTimeout(() => {
+        $(work).addClass("hide");
+      }, 500);
+      setTimeout(()=> {
+        if ($(work).hasClass(className)) {
+          $(work).removeClass("hide");
+          setTimeout(() => {
+            $(work).removeClass("hidden");
+          }, 500);
+        }
+      },500)
+      //todo Promise
+    });
+  };
+
+  $(buttons).each((i, toggleBtn) => {
+    $(toggleBtn).click(() => {
+      if ($(toggleBtn).hasClass("reactjs")) {
+        toggleView("reactjs");
+      } else if ($(toggleBtn).hasClass("html")) {
+        toggleView("html");
+      } else toggleView();
+    });
+  });
 });
+
+/* //! JS
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".portfolio-btn");
+  const works = document.querySelectorAll(".portfolio-wrap");
+
+  const toggleView = (className = "portfolio-wrap") => {
+    works.forEach((work) => {
+      work.classList.add("hidden");
+      work.classList.add("hide");
+      if (work.classList.contains(className)) {
+        work.classList.remove("hide");
+        work.classList.remove("hidden");
+      }
+    });
+  };
+
+  buttons.forEach((toggleBtn) => {
+    toggleBtn.addEventListener("click", () => {
+      if (toggleBtn.classList.contains("reactjs")) {
+        toggleView("reactjs");
+      } else if (toggleBtn.classList.contains("html")) {
+        toggleView("html");
+      } else toggleView();
+    });
+  });
+}); */
